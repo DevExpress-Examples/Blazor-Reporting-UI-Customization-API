be8c83fcd4f2b310ad40520e68e30ada83b8e837
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2020-10-14 15:15:25 +0300

    auto sync for version 2020.2

--- CS/BlazorReportingEvents/BlazorReportingEvents/Services/CustomReportStorageWebExtension.cs
+++ CS/BlazorReportingEvents/BlazorReportingEvents/Services/CustomReportStorageWebExtension.cs
@@ -17,13 +17,13 @@ namespace BlazorReportingEvents.Services {
             }
         }
 
-        private bool IsWithinReportsFolder(string url, string folder) {
+        private bool IsWithinReportsFolder(string reportName, string folder) {
             var rootDirectory = new DirectoryInfo(folder);
-            var fileInfo = new FileInfo(Path.Combine(folder, url));
+            var fileInfo = new FileInfo(Path.Combine(folder, reportName));
             return fileInfo.Directory.FullName.ToLower().StartsWith(rootDirectory.FullName.ToLower());
         }
 
-        public override bool CanSetData(string url) {
+        public override bool CanSetData(string reportName) {
             // Determines whether or not it is possible to store a report by a given URL. 
             // For instance, make the CanSetData method return false for reports that should be read-only in your storage. 
             // This method is called only for valid URLs (i.e., if the IsValidUrl method returned true) before the SetData method is called.
@@ -31,31 +31,31 @@ namespace BlazorReportingEvents.Services {
             return true;
         }
 
-        public override bool IsValidUrl(string url) {
+        public override bool IsValidUrl(string reportName) {
             // Determines whether or not the URL passed to the current Report Storage is valid. 
             // For instance, implement your own logic to prohibit URLs that contain white spaces or some other special characters. 
             // This method is called before the CanSetData and GetData methods.
 
-            return Path.GetFileName(url) == url;
+            return Path.GetFileName(reportName) == reportName;
         }
 
-        public override byte[] GetData(string url) {
+        public override byte[] GetData(string reportName) {
             // Returns report layout data stored in a Report Storage using the specified URL. 
             // This method is called only for valid URLs after the IsValidUrl method is called.
             try {
-                if(Directory.EnumerateFiles(ReportDirectory).Select(Path.GetFileNameWithoutExtension).Contains(url)) {
-                    return File.ReadAllBytes(Path.Combine(ReportDirectory, url + FileExtension));
+                if(Directory.EnumerateFiles(ReportDirectory).Select(Path.GetFileNameWithoutExtension).Contains(reportName)) {
+                    return File.ReadAllBytes(Path.Combine(ReportDirectory, reportName + FileExtension));
                 }
-                if(ReportsFactory.Reports.ContainsKey(url)) {
+                if(ReportsFactory.Reports.ContainsKey(reportName)) {
                     using(MemoryStream ms = new MemoryStream()) {
-                        ReportsFactory.Reports[url]().SaveLayoutToXml(ms);
+                        ReportsFactory.Reports[reportName]().SaveLayoutToXml(ms);
                         return ms.ToArray();
                     }
                 }
             } catch(Exception ex) {
                 throw new DevExpress.XtraReports.Web.ClientControls.FaultException("Could not get report data.", ex);
             }
-            throw new DevExpress.XtraReports.Web.ClientControls.FaultException(string.Format("Could not find report '{0}'.", url));
+            throw new DevExpress.XtraReports.Web.ClientControls.FaultException(string.Format("Could not find report '{0}'.", reportName));
         }
 
         public override Dictionary<string, string> GetUrls() {
@@ -69,21 +69,21 @@ namespace BlazorReportingEvents.Services {
                                      .ToDictionary<string, string>(x => x);
         }
 
-        public override void SetData(XtraReport report, string url) {
+        public override void SetData(XtraReport report, string reportName) {
             // Stores the specified report to a Report Storage using the specified URL. 
             // This method is called only after the IsValidUrl and CanSetData methods are called.
-            if(!IsWithinReportsFolder(url, ReportDirectory))
+            if(!IsWithinReportsFolder(reportName, ReportDirectory))
                 throw new DevExpress.XtraReports.Web.ClientControls.FaultException("Invalid report name.");
-            report.SaveLayoutToXml(Path.Combine(ReportDirectory, url + FileExtension));
+            report.SaveLayoutToXml(Path.Combine(ReportDirectory, reportName + FileExtension));
         }
 
-        public override string SetNewData(XtraReport report, string defaultUrl) {
+        public override string SetNewData(XtraReport report, string defaultReportName) {
             // Stores the specified report using a new URL. 
             // The IsValidUrl and CanSetData methods are never called before this method. 
             // You can validate and correct the specified URL directly in the SetNewData method implementation 
             // and return the resulting URL used to save a report in your storage.
-            SetData(report, defaultUrl);
-            return defaultUrl;
+            SetData(report, defaultReportName);
+            return defaultReportName;
         }
     }
 }
--- CS/BlazorReportingEvents/BlazorReportingEvents/wwwroot/js/reporting_DesignerCustomization.js
+++ CS/BlazorReportingEvents/BlazorReportingEvents/wwwroot/js/reporting_DesignerCustomization.js
@@ -1,15 +1,14 @@
 ﻿window.ReportingDesignerCustomization = {
     onCustomizeElements: function(s, e) {
         //Remove Menu button
-        var menuButton = e.GetById("dxrd-menubutton-template"); //"dxrd-menubutton-template" == DevExpress.Designer.Report.ReportDesignerElements.MenuButton
+        var menuButton = e.GetById(s.dx.Reporting.Designer.Utils.ReportDesignerElements.MenuButton)
         var menuButtonIndex = e.Elements.indexOf(menuButton);
         menuButtonIndex !== -1 && e.Elements.splice(menuButtonIndex, 1);
     },
 
-
     onCustomizeMenuActions: function(s, e) {
         //Custom New Report
-        var newReportAction = e.GetById("dxrd-newreport"); //"dxrd-newreport" == DevExpress.Designer.Report.ActionId.NewReport
+        var newReportAction = e.GetById(s.dx.Reporting.Designer.Actions.ActionId.NewReport);
         if(newReportAction) {
             newReportAction.clickAction = function(report) {
                 s.OpenReport("CustomNewReport");
@@ -23,7 +22,7 @@
         }
 
         //Move Save button to the toolbar
-        var saveAction = e.GetById("dxrd-save"); //"dxrd-save" == DevExpress.Designer.Report.ActionId.Save
+        var saveAction = e.GetById(s.dx.Reporting.Designer.Actions.ActionId.Save);
         saveAction.container = "toolbar";
         e.Actions.splice(e.Actions.indexOf(saveAction), 1);
         e.Actions.push(saveAction);
@@ -44,14 +43,12 @@
 
     onCustomizeWizard: function(s, e) {
         if(e.Type === "ReportWizard") {
-            e.Wizard.events.addHandler("beforePageInitialize", ReportingDesignerCustomization.onBeforePageInit)
-        }
-    },
-
-    onBeforePageInit: function(args) {
-        if(args.pageId === "selectReportTypePage") { // "selectReportTypePage" == DevExpress.Reporting.Designer.Wizard.FullscreenReportWizardPageId.SelectReportTypePage
-            args.page.typeItems.splice(0, 1);
-            args.page.typeItems.pop();
+            e.Wizard.events.addHandler("beforePageInitialize", (args) => {
+                if (args.pageId === s.dx.Reporting.Designer.Wizard.FullscreenReportWizardPageId.SelectReportTypePage) {
+                    args.page.typeItems.splice(0, 1);
+                    args.page.typeItems.pop();
+                }
+            })
         }
     }
 }
