a72db004de03e28751c98e908903e22ebd819c49
kanaev <sergey.kanayev@devexpress.com>
2024-11-28 18:32:07 +0400

    Fix event handler

--- CS/BlazorReportingEvents/BlazorReportingEvents/wwwroot/js/reporting_ConfigureCsrf.js
+++ CS/BlazorReportingEvents/BlazorReportingEvents/wwwroot/js/reporting_ConfigureCsrf.js
@@ -1,7 +1,7 @@
 ﻿window.ReportingConfigureCsrf = {
     onBeforeRender_configureCsrf: function (sender, e) {
         const token = document.getElementById('csrfToken').value;
-        e.dx.Analytics.Utils.fetchSetup.fetchSettings = {
+        sender.dx.Analytics.Utils.fetchSetup.fetchSettings = {
             headers: {
                 'RequestVerificationToken': `${token}`
             }
